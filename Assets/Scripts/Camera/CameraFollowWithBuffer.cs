47b8200b224e754af56f0900977264d39baa4529
DylanSchmidt-Cunningham <da_cunningham@laurentian.ca>
2018-02-25 21:39:06 -0500

    first area and camera transition

--- Assets/Scripts/Camera/CameraFollowWithBuffer.cs
+++ Assets/Scripts/Camera/CameraFollowWithBuffer.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 public class CameraFollowWithBuffer : MonoBehaviour {
 
     public Transform playerPosition;
-    public Transform playerForwardThreshold;
-    public Transform playerBackwardThreshold;
+    public Transform playerRightThreshold;
+    public Transform playerLeftThreshold;
+
+    private float rightOffset;
+    private float leftOffset;
 
     // Use this for initialization
     void Start () {
-        playerForwardThreshold = transform.GetChild(0);
-        playerBackwardThreshold = transform.GetChild(1);
+        playerRightThreshold = transform.GetChild(0);
+        playerLeftThreshold = transform.GetChild(1);
+
+        // calculate offsets of the forward and backward thresholds
+        rightOffset = playerRightThreshold.position.x - this.transform.position.x; // positive if right threshold is > camera position
+        leftOffset = this.transform.position.x - playerLeftThreshold.position.x; // positive if left threshold is < camera position
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(playerPosition.position.x > playerForwardThreshold.position.x || playerPosition.position.x < playerBackwardThreshold.position.x)
+		if(playerPosition.position.x > playerRightThreshold.position.x)
         {
-            this.transform.position = new Vector3(playerPosition.position.x, this.transform.position.y, this.transform.position.z);
+            this.transform.position = new Vector3(playerPosition.position.x + rightOffset, this.transform.position.y, this.transform.position.z);
+        }
+        else if (playerPosition.position.x < playerLeftThreshold.position.x)
+        {
+            this.transform.position = new Vector3(playerPosition.position.x - leftOffset, this.transform.position.y, this.transform.position.z);
         }
 	}
 
@@ -26,8 +37,8 @@ public class CameraFollowWithBuffer : MonoBehaviour {
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
-        drawThresholdLine(playerForwardThreshold);
-        drawThresholdLine(playerBackwardThreshold);
+        drawThresholdLine(playerRightThreshold);
+        drawThresholdLine(playerLeftThreshold);
     }
 
     // Draw a vertical line at a threshold
